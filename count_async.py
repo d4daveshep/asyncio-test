40343766fab1d4e0c9a775258f9e59c3924c156d
d4daveshep <david@daveshep.net.nz>
2022-10-18 17:21:21 +1300

    more testing

--- count_async.py
+++ count_async.py
@@ -4,30 +4,26 @@ import asyncio
 async def count12():
     while True:
         print("One")
-        await asyncio.sleep(1)  # await cedes control back to the event loop
+        await asyncio.sleep(.5)  # await cedes control back to the event loop
         print("Two")
 
 
 async def count34():
     while True:
         print("Three")
-        await asyncio.sleep(1)  # await cedes control back to the event loop
+        await asyncio.sleep(2)  # await cedes control back to the event loop
         print("Four")
 
 
-# async def main():
-#     await asyncio.gather(count12(), count34(), count12())  # this gathers the async functions to be run
+async def main():
+    await asyncio.gather(count12(), count34())  # this gathers the co-routines to be run
 
 
 if __name__ == "__main__":
 
-    loop = asyncio.get_event_loop()
     try:
-        asyncio.ensure_future(count12())
-        asyncio.ensure_future(count34())
-        loop.run_forever()
+        asyncio.run(main())
     except KeyboardInterrupt:
         pass
     finally:
         print("Closing loop")
-        loop.close()

f59a5a25e40559a474c2be56b5236c79b4d81b2b
d4daveshep <david@daveshep.net.nz>
2022-10-18 21:47:33 +1300

    async read target_temp from zmq and write to serial port

--- test_asyncio_reader_writer.py
+++ test_asyncio_reader_writer.py
@@ -1,11 +1,35 @@
 import asyncio
+import json
 import random
 
 import serial_asyncio
+import zmq as zmq
+from zmq.asyncio import Context, Poller
 
 
 async def open_serial_connection():
     return await serial_asyncio.open_serial_connection(url='/dev/ttyACM0', baudrate=115200)
+
+
+async def zmq_receiver(ctx: Context, url: str) -> None:
+    """receive messages with polling"""
+    pull = ctx.socket(zmq.PULL)
+    pull.connect(url)
+    poller = Poller()
+    poller.register(pull, zmq.POLLIN)
+    while True:
+        events = await poller.poll()
+        if pull in dict(events):
+            # print("recving", events)
+            msg = await pull.recv_multipart()
+            dict_received = json.loads(msg[0].decode("utf-8"))
+            print('received via zmq', dict_received)
+
+            try:
+                target_temp = dict_received["target-temp"]
+                await write_serial_string(writer, target_temp)
+            except KeyError as err_info:
+                print(f"{err_info} not received" )
 
 
 async def read_serial_line(reader):
@@ -14,32 +38,33 @@ async def read_serial_line(reader):
         print(str(line, 'utf-8'))
 
 
-async def write_serial_string(writer):
-    while True:
-        target_temp = random.randrange(10, 30)
-        string_to_write = '<' + str(target_temp) + '>'
-        print(f"writing {string_to_write}")
-        writer.write(string_to_write.encode())
-        await asyncio.sleep(5)
-
-
-async def main(reader, writer):
-    # await asyncio.gather(read_serial_line(reader), write_serial_string(writer))
-    await asyncio.gather(read_serial_line(reader))
+async def write_serial_string(writer, temp: float):
+    # while True:
+    # target_temp = random.randrange(10, 30)
+    string_to_write = '<' + str(temp) + '>'
+    print(f"writing {string_to_write} to serial")
+    writer.write(string_to_write.encode())
+    await asyncio.sleep(0)
 
 
 if __name__ == "__main__":
 
+    url = 'tcp://127.0.0.1:5555'
+
+    ctx = Context.instance()
+
     loop = asyncio.get_event_loop()
 
     try:
-
         reader, writer = loop.run_until_complete(open_serial_connection())
-        writer_task = loop.create_task(write_serial_string(writer))
+
+        receiver_task = loop.create_task(zmq_receiver(ctx, url))
+
+        # writer_task = loop.create_task(write_serial_string(writer))
         reader_task = loop.create_task(read_serial_line(reader))
+
         loop.run_forever()
 
-        # asyncio.run(main(reader, writer))
     except KeyboardInterrupt:
         print("Stopping loop")
         loop.stop()
